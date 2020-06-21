7dd7e5613c0957283da4749c71da0b7e7973d8dc
Steven Foerster <steven@cyber5k.com>
2020-06-21 00:17:12 -0400

    dependencies function

--- cli/wifi.py
+++ cli/wifi.py
@@ -2,6 +2,9 @@ import sys
 import os
 import subprocess
 from plumbum import cli
+        
+def get_wifi_ifaces():
+    return [iface.strip() for iface in subprocess.check_output('iwconfig 2>/dev/null | grep -e "^\w" | awk \'{print $1}\'', shell=True).decode().splitlines()]
 
 class MistbornWifi(cli.Application):
     """
@@ -11,6 +14,13 @@ class MistbornWifi(cli.Application):
         """
         Main function for Mistborn managing Access Point.
         """
-        wifi_ifaces = subprocess.check_output('iwconfig 2>/dev/null | grep -e "^\w" | awk \'{print $1}\'', shell=True).decode().strip() 
+        wifi_ifaces = get_wifi_ifaces()
         print(f"WIFI ifaces: {wifi_ifaces}")
 
+    def dependencies(self):
+        """
+        Install system dependencies.
+        """
+        # hostapd
+        # stop and disable services
+        os.system("sudo apt-get update && sudo apt-get install -y hostapd && sudo systemctl stop hostapd && sudo systemctl disable hostapd")
